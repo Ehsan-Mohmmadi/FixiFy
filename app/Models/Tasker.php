e8bce63f6af7ed501930a03217fed54a242f458e
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-10-11 12:38:52 +0330

    Update Tasker.php
    Add fields to Tasker Model

--- app/Models/Tasker.php
+++ app/Models/Tasker.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Tasker extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+        'full_name',
+        'email',
+        'profile_pic',
+        'job_title',
+        'cost',
+        'work_time',
+        'description',
+        'total_rate',
+        'work_quality',
+        'reliability',
+        'punctuality',
+        'solution',
+        'payout'
+
+
+    ];
 }
