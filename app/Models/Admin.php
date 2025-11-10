6df59ad6c3af018af76a00d56d883e08dc79501e
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-11-10 21:31:05 +0330

    Update Admin.php
    add fillable

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Model
 {
+    use HasApiTokens;
     use HasFactory;
+
+    protected $fillable = [
+      'name',
+      'last_name',
+      'national_code',
+    ];
 }
+
