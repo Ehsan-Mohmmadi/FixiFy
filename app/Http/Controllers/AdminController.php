0f0ab2bf62613816681a1ab5c96200e77e7695b5
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-11-10 20:57:26 +0330

    Update AdminController.php
    Add store function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AdminStoreRequest;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function store(AdminStoreRequest $adminStoreRequest)
+    {
+        dd($adminStoreRequest->all());
+    }
 }
