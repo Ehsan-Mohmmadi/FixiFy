148cdfbe81527a0541ab969aee64a7db626b7d60
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-11-10 20:59:16 +0330

    Update 2025_11_10_171228_create_admins_table.php
    Add name,last_name,national_code

--- database/migrations/2025_11_10_171228_create_admins_table.php
+++ database/migrations/2025_11_10_171228_create_admins_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('national_code');
             $table->timestamps();
         });
     }
