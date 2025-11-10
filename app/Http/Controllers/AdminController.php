b129dc3eb87cbd69a65f31f6b46ada72a0bdae7f
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-11-10 21:30:46 +0330

    Update AdminController.php
    complete store function
    add show function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AdminStoreRequest;
+use App\Http\Resources\AdminResource;
+use App\Models\Admin;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function store(AdminStoreRequest $adminStoreRequest)
     {
-        dd($adminStoreRequest->all());
+        $admin = Admin::create($adminStoreRequest->all());
+        $token = $admin->createToken('Api/AdminLogin');
+        return response()->json([
+            'message' => 'admin created successfully',
+            'token' => $token->plainTextToken,
+            'data' => new AdminResource($admin)
+        ],200);
+    }
+
+    public function show(Admin $admin)
+    {
+        return response()->json([
+            'message' => 'Admin data fetch successfully',
+            'data' => new AdminResource($admin)
+        ],200);
     }
 }
