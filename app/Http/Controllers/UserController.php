f3c525db0aa5e84b60484cad0745b536bdb5128e
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-11-12 08:36:00 +0330

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,50 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserShowRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function register(Request $request){
-        $incomingFields = $request->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required',
-        ]);
-        $incomingFields['password'] = bcrypt($incomingFields['password']);
-        $user = User::create($incomingFields);
-        auth()->login($user);
-        return response()->json([
-            'message' => 'Successfully created user!',
-            'message2' => 'You are logged in!',
-            'user' => $user
-        ],200);
-    }
+//    public function register(Request $request){
+//        $incomingFields = $request->validate([
+//            'name' => 'required',
+//            'email' => 'required|email|unique:users',
+//            'password' => 'required',
+//        ]);
+//        $incomingFields['password'] = bcrypt($incomingFields['password']);
+//        $user = User::create($incomingFields);
+//        return response()->json([
+//            'message' => 'Successfully created user!',
+//            'user' => $user
+//        ],200);
+//    }
+//
+//    public function login(Request $request)
+//    {
+//        $incomingFields = $request->validate([
+//            'email' => 'required|email',
+//            'password' => 'required',
+//        ]);
+//        if (!auth()->attempt($incomingFields)) {
+//            return response()->json([
+//               'message' => 'Unauthorized'
+//            ]);
+//        }
+//        else{
+//            return response()->json([
+//                'message' => 'Successfully logged in'
+//            ]);
+//        }
+//    }
+//
+//    public function logout(Request $request)
+//    {
+//        auth()->logout();
+//        return response()->json([
+//            'message' => 'Successfully logged out'
+//        ],200);
+//    }
 
-    public function login(Request $request)
+    public function userAdd(UserShowRequest $userShowRequest)
     {
-        $incomingFields = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-        if (!auth()->attempt($incomingFields)) {
-            return response()->json([
-               'message' => 'Unauthorized'
-            ]);
-        }
-        else{
-            return response()->json([
-                'message' => 'Successfully logged in'
-            ]);
-        }
-    }
-
-    public function logout(Request $request)
-    {
-        auth()->logout();
+        $user = User::create($userShowRequest->all());
+        $token = $user->createToken('Api/UserLogin');
         return response()->json([
-            'message' => 'Successfully logged out'
+            'message' => 'User created successfully',
+            'token' => $token->plainTextToken,
+            'data' => new UserResource($user)
         ],200);
     }
 }
